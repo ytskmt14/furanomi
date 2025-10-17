ffdeaa2cf7db253f5a3e2299b93e8b855f50df9a
y.sakamoto <y.sakamoto@lu-llc.jp>
2025-10-17 15:51:51 +0900

    feat: .gitignoreファイル追加
    - node_modules、dist、.env等を除外
    - IDE設定ファイルを除外
    - OS固有ファイルを除外
    - ログファイルを除外

---
+++ backend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+  },
+};
